ec1fbafbe26bf1d0ce950f1211d103ba79246ce7
EmilDok <kyrbicanal2@gmail.com>
2024-06-04 02:35:14 +0300

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,6 @@
+def avatar_url_for(user, opts = {})
+  size = opts[:size || 32]
+
+  hash = Digest::MD5.hexdigest(user.email.downcase)
+  "https://secure.gravatar.com/avatar/#{hash}.png?s=#{size}"
+end
